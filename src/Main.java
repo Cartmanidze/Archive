d212904cb50aaaf519aa113db2539e2e95741beb
Cartmanidze <glebon84@gmail.com>
2017-09-14 13:20:59 +0300

    thrid commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[]args)
     {
-        System.out.println("Suka");
-        System.out.println("aa");
+        System.out.println("Cartmanidze");
+
     }
 }
